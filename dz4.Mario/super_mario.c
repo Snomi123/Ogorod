d57b1eb4d33c79d60d91ed5adefbb53603faf8f9
Igor Maiorov <igormaiorov4@yandex.ru>
2025-10-06 23:45:57 +0300

    full IMBA

--- dz4.Mario/super_mario.c
+++ dz4.Mario/super_mario.c
@@ -13,6 +13,7 @@ typedef struct SObject {
     float width, height;
     float vertSpeed;
     bool IsFly;
+    char cType;
 } TObject;
 
 char map[mapHeight][mapWidth + 1];
@@ -22,7 +23,7 @@ int brickLength;
 
 void ClearMap() {
     for (int i = 0; i < mapWidth; i++)
-        map[0][i] = '.';
+        map[0][i] = ' ';
     map[0][mapWidth] = '\0';
     for (int j = 1; j < mapHeight; j++)
         snprintf(map[j], sizeof(map[j]), "%s", map[0]);
@@ -41,25 +42,26 @@ void SetObjectPos(TObject *obj, float xPos, float yPos) {
     obj->y = yPos;
 }
 
-void InitObject(TObject *obj, float xPos, float yPos, float oWidth, float oHeight) {
+void InitObject(TObject *obj, float xPos, float yPos, float oWidth, float oHeight, char inType) {
     SetObjectPos(obj, xPos, yPos);
     obj->width = oWidth;
     obj->height = oHeight;
     obj->vertSpeed = 0;
+    obj->cType = inType;
 }
 
 bool IsCollision(TObject o1, TObject o2);
 
 void CreateLevel(){
-    InitObject(&mario, 39, 10, 3, 3);
+    InitObject(&mario, 39, 10, 3, 3, '@');
     
     brickLength = 5;
-    brick = malloc(sizeof(*brick) * brickLength);
-    InitObject(&brick[0], 20, 20, 40, 5);
-    InitObject(&brick[1], 60, 15, 10, 10);
-    InitObject(&brick[2], 80, 20, 20, 5);
-    InitObject(&brick[3], 120, 15, 10, 10);
-    InitObject(&brick[4], 150, 20, 40, 5);
+    brick = realloc(brick, sizeof(*brick) * brickLength);
+    InitObject(&brick[0], 20, 20, 40, 5, '#');
+    InitObject(&brick[1], 60, 15, 10, 10, '#');
+    InitObject(&brick[2], 80, 20, 20, 5, '#');
+    InitObject(&brick[3], 120, 15, 10, 10, '#');
+    InitObject(&brick[4], 150, 20, 40, 5, '#');
 }
 
 void VertMoveObject(TObject *obj){
@@ -106,7 +108,7 @@ void PutObjectOnMap(TObject obj) {
     for (int i = ix; i < (ix + iWidth); i++) {
         for (int j = iy; j < (iy + iHeight); j++) {
             if (IsPosInMap(i,j))
-                map[j][i] = '@';
+                map[j][i] = obj.cType;
         }
     }
 }
