4594baab7ddb0d29c3d97652d7ce8148bdeac8d8
Igor Maiorov <igormaiorov4@yandex.ru>
2025-10-06 20:31:35 +0300

    Create Cursor and MacOs

--- dz4.Mario/mari.c
+++ dz4.Mario/mari.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <math.h>
-#include <curses.h>
+#include <ncurses.h>
 
 #define mapWidth 80
 #define mapHeight 25
 
-
-typedef struct SObject;{
+typedef struct SObject {
     float x,y;
     float width, height;
 } TObject;
@@ -17,7 +15,6 @@ char map[mapHeight][mapWidth+1];
 TObject mario;
 
 void ClearMap(){
-    
     for (int i = 0; i < mapWidth; i++)
         map[0][i] = '.';
     map[0][mapWidth] = '\0';
@@ -28,7 +25,7 @@ void ClearMap(){
 void ShowMap(){
     map[mapHeight - 1][mapWidth - 1] = '\0';
     for (int j = 0; j < mapHeight; j++)
-        printf("%s", map[j]);
+        printw("%s\n", map[j]);  // Изменено на printw для ncurses
 }
 
 void SetObjectPos(TObject *obj, float xPos, float yPos){
@@ -36,37 +33,51 @@ void SetObjectPos(TObject *obj, float xPos, float yPos){
     (*obj).y = yPos;
 }
 
-
-void InitObject(TObject, *obj, float xPos, float yPos, float oWidth, float oHeigth){
+void InitObject(TObject *obj, float xPos, float yPos, float oWidth, float oHeight){  // Исправлено: oHeigth -> oHeight
     SetObjectPos(obj, xPos, yPos);
     (*obj).width = oWidth;
     (*obj).height = oHeight;
-    
-}
-
-void PutObjectPos(TObject obj){
-    int ix = (int)round(obj.x);
-    int iy = (int)round(obj.x);
-    int iWidth = (int)round(obj.width);
-    int Height = (int)round(obj.height);
-    
-    for (int i = ix, i < (ix + iWidth); i++)
-        for (int j = iy; j < (iy + iHeight); j++)
-            map[j][i] = '@';
 }
 
 void PutObjectOnMap(TObject obj){
     int ix = (int)round(obj.x);
-    int iy = (int)round(obj.y);
+    int iy = (int)round(obj.y);  // Исправлено: было round(obj.x) вместо obj.y
+    int iWidth = (int)round(obj.width);
+    int iHeight = (int)round(obj.height);  // Исправлено: Height -> iHeight
+    
+    for (int i = ix; i < (ix + iWidth); i++)  // Исправлено: добавил ;
+        for (int j = iy; j < (iy + iHeight); j++)
+            map[j][i] = '@';
+    
     map[iy][ix] = '@';
 }
 
 int main(){
-    hide_cursor();
+    // Инициализация ncurses
+    initscr();
+    keypad(stdscr, TRUE);
+    noecho();
+    curs_set(0);  // Скрыть курсор (аналог hide_cursor())
+    timeout(0);   // Неблокирующий ввод
     
     InitObject(&mario, 39, 10, 3, 3);
-    ClearMap();
-    PutObjectOnMap(mario);
-    ShowMap();
+    
+    bool running = true;
+    while (running) {
+        // Проверка нажатия ESC
+        int ch = getch();
+        if (ch == 27) {  // 27 - код клавиши ESC
+            running = false;
+        }
+        
+        ClearMap();
+        PutObjectOnMap(mario);
+        clear();    // Очистка экрана ncurses
+        ShowMap();
+        refresh();  // Обновление экрана ncurses
+        napms(50);  // Небольшая задержка для снижения нагрузки на CPU
+    }
+    
+    endwin();  // Завершение работы с ncurses
     return 0;
 }
